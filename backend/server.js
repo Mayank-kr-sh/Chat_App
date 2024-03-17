31344a661a50466a4efd04822acb2f3e37d8ce7b
Mayank Kumar Shah <mayank.k.iiit@gmail.com>
2024-03-17 19:03:23 +0530

    Ready For Deployment

--- backend/server.js
+++ backend/server.js
@@ -6,21 +6,36 @@ const cors = require("cors");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const chatRoutes = require("./Routes/chatRoutes");
 const messageRoutes = require("./Routes/messageRoutes");
+const path = require("path");
 
 require("dotenv").config();
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json()); // to parse the body of the request and get the data in json format
 
-app.get("/", (req, res) => {
-  res.send("API is running ...");
-});
-
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+// ******************Deployments*********************
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// ******************Deployments*********************
 
 app.use(notFound);
 app.use(errorHandler);
